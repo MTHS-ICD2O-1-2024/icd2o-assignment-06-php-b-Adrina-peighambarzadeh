91a5800573ff3eff7e52a5117383a290d2218508
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-05-29 16:49:29 +0000

    init

--- answer.php
+++ answer.php
@@ -7,7 +7,6 @@
   <meta name="keywords" content="mths, icd2o" />
   <meta name="author" content="Adrina Peighambarzadeh" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta http-equiv="refresh" content="5;url=index.html" />
   <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
   <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.brown-deep_orange.min.css" />
