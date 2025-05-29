d70794703233b2e4217c02c24cf864903ff96660
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-05-29 16:52:48 +0000

    init

--- answer.php
+++ answer.php
@@ -3,14 +3,14 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Random Motivational Quotes, with PHP (API)" />
+  <meta name="description" content="Motivational Phrases, with PHP (API)" />
   <meta name="keywords" content="mths, icd2o" />
   <meta name="author" content="Adrina Peighambarzadeh" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
   <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.brown-deep_orange.min.css" />
-  <title>Motivational Bible Phrases, with PHP (API)</title>
+  <title>Motivational Phrases, with PHP (API)</title>
 </head>
 
 <body>
@@ -18,7 +18,7 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Motivational Bible Phrases, with PHP (API)</span>
+        <span class="mdl-layout-title">Motivational Phrases, with PHP (API)</span>
       </div>
     </header>
     <main class="mdl-layout__content">
@@ -40,15 +40,15 @@
 
             echo '<b>Quote:</b> "' . $quotePrase . '"<br />';
             echo '<b>Author:</b> ' . $quoteAuthor . '<br />';
+          }
         }
-      }
         ?>
       </div>
-        <a href="./index.php" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-          Return to Home Now
-        </a>
-      </div>
-    </main>
+      <a href="./index.php" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+        Return to Home Now
+      </a>
+  </div>
+  </main>
   </div>
 </body>
 
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Random Motivational Quotes, with PHP (API)" />
+  <meta name="description" content="Motivational Phrases, with PHP (API)" />
   <meta name="keywords" content="mths, icd2o" />
   <meta name="author" content="Adrina peighambarzadeh" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -14,7 +14,7 @@
   <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
   <link rel="manifest" href="site.webmanifest" />
-  <title>Motivational Bible Phrases, with PHP (API)</title>
+  <title>Motivational Phrases, with PHP (API)</title>
 </head>
 
 <body>
@@ -22,7 +22,7 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Motivational Bible Phrases, with PHP (API)</span>
+        <span class="mdl-layout-title">Motivational Phrases, with PHP (API)</span>
       </div>
     </header>
     <main class="mdl-layout__content">
