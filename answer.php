f751e7ee0b959333c3829e874ec03658544fc18a
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-05-29 16:47:51 +0000

    init

--- answer.php
+++ answer.php
@@ -45,7 +45,7 @@
       }
         ?>
       </div>
-        <a href="./index.html" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+        <a href="./index.php" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
           Return to Home Now
         </a>
       </div>
